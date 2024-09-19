5502dc880376231b5a2f5f0aa7cdf753d1ccd7b6
Sina Abedi <Sinaabediofficial@gmail.com>
2024-09-19 21:48:31 +0330

    update style

--- src/components/templates/SendOtpForm.jsx
+++ src/components/templates/SendOtpForm.jsx
@@ -13,27 +13,48 @@ function SendOtpForm({ mobile, setStep, setMobile }) {
   return (
     <form
       onSubmit={submitHandler}
-      className="container max-w-lg m-auto flex flex-col mt-24 border-2 border-solid border-gray-300 rounded-md p-1"
+      className="container max-w-max m-auto flex flex-col mt-24 border-2 border-solid border-gray-300 rounded-md p-1"
     >
-      <p className="text-lg font-normal mb-5">ورود به حساب کاربری</p>
-      <span className="text-gray-400 text-sm mb-5">
-        برای استفاده از امکانات دیوار ، لطفا شماره موبایل خود را وارد کنید. کد
-        تایید به این شماره پیامک خواهد شد
+      <p className="text-lg font-normal mb-6 border-b-2 pb-6 p-5">
+        ورود به حساب کاربری
+      </p>
+      <label htmlFor="input" className="p-5 mb-6 text-lg">
+        شماره موبایل خود را وارد کنید
+      </label>
+      <span className="text-gray-500 text-sm mb-3 pr-5 font-extralight">
+        قبل از ثبت آگهی، لطفا وارد حساب خود شوید.
       </span>
-      <label htmlFor="input">شماره موبایل خود را وارد کنید</label>
-      <input
-        className="m-4 p-1 border-2 border-solid border-gray-300 rounded-md"
-        type="text"
-        id="input"
-        placeholder="شماره موبایل"
-        value={mobile}
-        onChange={(e) => setMobile(e.target.value)}
-      />
+      <span className="text-gray-500 text-sm pr-5 font-extralight">
+        کد تایید به این شماره پیامک می شود.
+      </span>
+      <div className="relative flex">
+        <input
+          className="m-4  w-full px-1 py-2 mx-6 border-2 border-solid pr-5 border-gray-300 rounded-md placeholder:font-extralight outline-red-700 bg-inherit"
+          type="tel"
+          id="input"
+          placeholder="شماره موبایل"
+          value={mobile}
+          onChange={(e) => setMobile(e.target.value)}
+        />
+        <p className="w-12 absolute bg-gray-100 items-center justify-center text-center rounded-2xl px-4 py-1 font-extralight text-sm">
+          ۹۸+
+        </p>
+      </div>
+      <p className="text-gray-500 text-base font-extralight px-6">
+        <a href="" className="text-red-700">
+          شرایط استفاده از خدمات{" "}
+        </a>
+        و
+        <a href="" className="text-red-700">
+          حریم خصوصی{" "}
+        </a>
+        دیوار را می پذیرم.
+      </p>
       <button
-        className="w-16 px-1 py-3 border-none bg-red-800 text-white rounded-md cursor-pointer"
+        className="w-28 py-2 border-none bg-red-700  text-white rounded-md text-lg cursor-pointer hover:bg-red-800 hover:transition-colors"
         type="submit"
       >
-        ارسال کد تایید
+        تأیید
       </button>
     </form>
   );
