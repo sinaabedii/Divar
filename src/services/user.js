a5cb07d4690fe691800439f7f36d2c7c1d55084f
sina-abedi <110241278+sinaabedii@users.noreply.github.com>
2024-10-08 14:13:47 +0330

    Update user.js

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,7 @@
 import api from "../configs/api";
 
 const getProfile = () =>
-  api.get("user/whoami");
+  api.get("user/whoami").then((response) => response || false); //error profile
 const getPosts = () => api.get("post/my");
 const getAllPosts = () => api.get("");
 
